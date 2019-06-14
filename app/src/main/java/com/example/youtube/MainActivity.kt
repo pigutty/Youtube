4e1bc221e88125a22412f7bc9c65fff2a61730e9
pigutty <50047826+pigutty@users.noreply.github.com>
2019-06-14 20:41:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.youtube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle as Bundle1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
