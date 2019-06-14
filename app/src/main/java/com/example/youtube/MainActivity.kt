d4ebf126a23daf1338f146a1ff12b5d35a36eeda
pigutty <50047826+pigutty@users.noreply.github.com>
2019-06-14 21:02:42 +0900

    Section 2 video 2

--- app/src/main/java/com/example/youtube/MainActivity.kt
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.youtube
 
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle as Bundle1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener{
+            // code
+            Log.i("MainActivity", "Button was clicked !")
+
+            Toast.makeText(this , "Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
     }
 }
